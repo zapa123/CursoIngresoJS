131fecceb38e1c8b5ae2218748552b03fe9e8543
zapa123 <scaladominguez@gmail.com>
2018-07-03 11:58:58 -0300

    Ejercicio 1 y 2.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,8 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	
+    var mensaje = "esto funciona de maraviila";
+    alert("esto funciona de maraviila");
+   
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,11 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+    var nombre = prompt ("escriba su nombre");
+    alert ("su nombre es " + nombre);
+    
+
+
 	
 }
 
